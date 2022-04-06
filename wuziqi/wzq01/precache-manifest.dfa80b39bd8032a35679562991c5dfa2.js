a7c4011ae4ec0f3f4d08cb1e5ac8ce50587336ba
tom-cf <uipps_com@163.com>
2022-04-06 23:11:35 +0800

    五子棋；公共头修改

---
+++ wuziqi/wzq01/precache-manifest.dfa80b39bd8032a35679562991c5dfa2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b94c10f5078dc21c4e8d",
+    "url": "/wuziqi/wzq01/ai.bundle.js"
+  },
+  {
+    "revision": "396997638e15c497e592",
+    "url": "/wuziqi/wzq01/css/app.58e2959d.css"
+  },
+  {
+    "revision": "396997638e15c497e592",
+    "url": "/wuziqi/wzq01/app.bundle.js"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/wuziqi/wzq01/img/sad.ffff4377.gif"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/wuziqi/wzq01/img/haha.9ad347b5.gif"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "/wuziqi/wzq01/img/board.5442f90f.jpg"
+  },
+  {
+    "revision": "bd4074dacc3e74123ca169bb42568ef1",
+    "url": "/wuziqi/wzq01/index.html"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/wuziqi/wzq01/img/haha.gif"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/wuziqi/wzq01/img/sad.gif"
+  }
+];
